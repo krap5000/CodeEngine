4feff6099f1052ec28934944ad102ba8ab9b32c7
krap5000 <krap5000@gmail.com>
2021-02-16 15:19:27 -0500

    Update server.js

--- hello/server.js
+++ hello/server.js
@@ -4,8 +4,8 @@ http.createServer(function (request, response) {
    target = process.env.TARGET ? process.env.TARGET : 'World' ;
    msg = process.env.MSG ? process.env.MSG : 'Hello Hi ' + target + '\n';
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   respons.write(msg);
-   response.end(msg);
+   response.write(msg);
+   response.end();
 }).listen(8080);
 
 console.log('Server running at http://0.0.0.0:8080/');
